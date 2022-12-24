83df839f618f77a1979ec242f904dca2cd11bfea
hajar-zarguan <hajar.zarguan.0610@gmail.com>
2022-12-24 20:52:30 +0100

    Infraction side of command with security updates

---
+++ infraction-command-side/src/main/java/com/example/infractioncommandside/InfractionCommandSideApplication.java
@@ -0,0 +1,13 @@
+package com.example.infractioncommandside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfractionCommandSideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfractionCommandSideApplication.class, args);
+    }
+
+}
---
+++ infraction-command-side/src/test/java/com/example/infractioncommandside/InfractionCommandSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infractioncommandside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfractionCommandSideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
